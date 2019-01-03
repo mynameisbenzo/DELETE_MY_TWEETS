1588732023a811489bfbed6924f953c7b2fb4c6f
Lorenzo Hernandez <lorenzohernandez@Lorenzos-MacBook-Air.local>
2019-01-02 21:22:37 -0800

    git ignore

--- twitter_clear.py
+++ twitter_clear.py
@@ -2,12 +2,15 @@ import selenium
 
 from selenium import webdriver
 
+print(__dir__)
 
 driver = webdriver.Chrome()
 driver.get("https://www.twitter.com/login")
 
 un_input = driver.find_element_by_class_name("js-username-field")
 pw_input = driver.find_element_by_class_name("js-password-field")
+
+
 
 print(un_input)
 print(pw_input)
